0d065c0ae348fe1bf1de34a994b23922ded1390a
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-08 09:14:26 -0400

    commit 2

--- src/main/java/me/moosecanswim/favrecip/controller/MainController.java
+++ src/main/java/me/moosecanswim/favrecip/controller/MainController.java
@@ -1,8 +1,25 @@
 package me.moosecanswim.favrecip.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/cornbread")
+    public String cornbread(){
+        return "cornbread";
+    }
+    @GetMapping("/crabcakes")
+    public String crabcakes(){
+        return "crabcakes";
+    }
+    @GetMapping("/gingerale")
+    public String gingerale(){
+        return "gingerale";
+    }
 }
